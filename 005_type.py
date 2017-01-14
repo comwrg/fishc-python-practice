535cd35463ab1a699c2af2d17da0fa9e7fb4f6e6
comwrg <comwrg@qq.com>
2017-01-14 23:35:50 +0800

    Add note

--- 005_type.py
+++ 005_type.py
@@ -8,3 +8,16 @@
 @time: 2017/1/14 23:13
 """
 
+# e记法
+e = 15e10
+print(e) # 150000000000.0
+
+# get type
+i = 0
+print(type(i)) # <class 'int'>
+f = 0.1
+print(type(f)) # <class 'float'>
+
+# type is equal
+s = 'fishc'
+print(isinstance(s, str)) # True
