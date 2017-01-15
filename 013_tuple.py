4b646dc62c56bc5d11fc6c246140d021a23c689e
comwrg <comwrg@qq.com>
2017-01-15 18:39:24 +0800

    Add note

--- 013_tuple.py
+++ 013_tuple.py
@@ -8,3 +8,14 @@
 @time: 2017/1/15 18:25
 """
 
+tuple1 = (1, 2)
+print(tuple1)
+print(tuple1[0])
+
+temp = (1)
+print(type(temp)) # <class 'int'>
+temp = (1, )
+print(type(temp)) # <class 'tuple'>
+
+print(8 * (8)) # 64
+print(8 * (8, )) # (8, 8, 8, 8, 8, 8, 8, 8)
