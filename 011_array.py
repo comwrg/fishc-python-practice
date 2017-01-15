261597d6229c1bc24468b7fbb140d7dbfd6cd24d
comwrg <comwrg@qq.com>
2017-01-15 17:55:46 +0800

    Add note

--- 011_array.py
+++ 011_array.py
@@ -8,3 +8,18 @@
 @time: 2017/1/15 17:46
 """
 
+arr = [0, 1, 2, 3, 4, 5]
+# first method
+arr.remove(0)
+# second method
+del arr[0]
+# third method
+arr = [0, 1, 2, 3, 4, 5]
+n = arr.pop()
+print(arr)
+print(n)
+
+arr = [0, 1, 2, 3, 4, 5]
+print(arr[1:3]) # [1, 2]
+print(arr[:3]) # [0, 1, 2]
+print(arr[:]) # new copy
