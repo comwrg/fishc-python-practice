463e3bc3d8033a07a4e50c8967cce8b405b7bebf
comwrg <comwrg@qq.com>
2017-01-19 13:15:44 +0800

    add 017_function note

---
+++ 017_function.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: comwrg
+@license: MIT
+@file: 017_function.py
+@time: 2017/1/19 13:11
+"""
+
+def demo():
+    print('demo')
+
+demo()
+
+
+def add(a, b):
+    return a + b
+
+print(add(1, 2))
+
