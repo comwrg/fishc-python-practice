b6bf23e701099e7fabc0b84e60528a8ea35e45b3
comwrg <comwrg@qq.com>
2017-02-04 13:20:54 +0800

    add 025 note.

---
+++ 025_dict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: comwrg
+@license: MIT
+@file: 025_dict.py
+@time: 2017/2/4 13:14
+"""
+
+dict1 = {
+    '李宁': '一切皆有可能',
+    'fishc': '让编程改变世界'
+}
+print(dict1['fishc'])
+
+dict1 = dict(((1, 1), (2, 2)))
+print(dict1)
+
